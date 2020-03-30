456f3326c97a98e9fc41b9c8c649b9553cece98e
Tom Osika <tommycollege558@gmail.com>
2020-03-30 13:23:28 -0400

    fix overload casts in polygon bindings

--- python/kwiver/vital/types/polygon.cxx
+++ python/kwiver/vital/types/polygon.cxx
@@ -47,15 +47,13 @@ PYBIND11_MODULE(polygon, m)
 {
   py::class_< kv::polygon, std::shared_ptr< kv::polygon > >(m, "Polygon")
   .def(py::init<>())
-  .def(py::init<const std::vector< kv::polygon::point_t >&>())
-  .def("push_back",
-    static_cast<void (kv::polygon::*) (double, double)>(&kv::polygon::push_back))
-  .def("push_back",
-    static_cast<void (kv::polygon::*) (const kv::polygon::point_t&)>(&kv::polygon::push_back))
+  .def(py::init< const std::vector< kv::polygon::point_t >&>())
+  .def("push_back", (void (kv::polygon::*) (double, double)) (&kv::polygon::push_back))
+  .def("push_back", (void (kv::polygon::*) (const kv::polygon::point_t&)) (&kv::polygon::push_back))
   .def("num_vertices", &kv::polygon::num_vertices)
   .def("get_vertices", &kv::polygon::get_vertices)
-  .def("contains", static_cast<bool (kv::polygon::*) (double, double)>(&kv::polygon::contains))
-  .def("contains", static_cast<bool (kv::polygon::*) (const kv::polygon::point_t&)>(&kv::polygon::contains))
+  .def("contains", (bool (kv::polygon::*) (double, double)) (&kv::polygon::contains))
+  .def("contains", (bool (kv::polygon::*) (const kv::polygon::point_t&)) (&kv::polygon::contains))
   .def("at", &kv::polygon::at)
   ;
 }
