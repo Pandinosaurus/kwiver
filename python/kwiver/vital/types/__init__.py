6523e65298720528d10dbd205e5f60db31225d04
Tom Osika <tommycollege558@gmail.com>
2020-04-27 17:06:41 -0400

    Add bindings and tests for fundamental_matrix

--- python/kwiver/vital/types/__init__.py
+++ python/kwiver/vital/types/__init__.py
@@ -17,6 +17,7 @@ from kwiver.vital.types.detected_object import *
 from kwiver.vital.types.detected_object_set import *
 from kwiver.vital.types.feature import *
 from kwiver.vital.types.feature_set import *
+from kwiver.vital.types.fundamental_matrix import *
 from kwiver.vital.types.geodesy import *
 from kwiver.vital.types.geo_covariance import *
 from kwiver.vital.types.geo_MGRS import *
