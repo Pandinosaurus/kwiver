a2cc9b9eb27d30452b2fb287cc322868340a7dac
Tom Osika <tommycollege558@gmail.com>
2020-06-11 11:32:14 -0400

    Fix copyright in new/modified files

--- python/kwiver/vital/tests/test_config.py
+++ python/kwiver/vital/tests/test_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #ckwg +28
-# Copyright 2019 by Kitware, Inc.
+# Copyright 2019-2020 by Kitware, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
